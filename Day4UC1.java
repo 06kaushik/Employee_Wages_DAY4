bf82b427ab63322bf65948948479a9e328ef6d43
06kaushik <himanshu06kaushik@gmail.com>
2021-04-15 14:27:34 +0530

    Welcome to snake and ladder

---
+++ Day4UC1.java
@@ -0,0 +1,7 @@
+public class snakeLadder {
+         public static void main(String[] args) {
+                System.out.println("Welcome to snake and ladder game");
+		int startPosition = 0;
+		System.out.println("player starts at position " + startPosition);
+	}
+}
